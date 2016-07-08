f27898f71f87781ee7f6a10974113149b056a591
Spiderlover <darthvader438@gmail.com>
2016-07-08 19:04:04 -0400

    Game Files

---
+++ Panda3D-1.10.0/python/Scripts/pywrap.bat
@@ -0,0 +1,3 @@
+@echo off
+
+start "" "C:\Panda3D-1.10.0\python\pythonw.exe" "C:\Panda3D-1.10.0\python\Scripts\pywrap" %1 %2 %3 %4 %5 %6 %7 %8 %9
---
+++ Panda3D-1.10.0/python/Scripts/pywxrc.bat
@@ -0,0 +1,3 @@
+@echo off
+
+"C:\Panda3D-1.10.0\python\python.exe" "C:\Panda3D-1.10.0\python\Scripts\pywxrc" %1 %2 %3 %4 %5 %6 %7 %8 %9
